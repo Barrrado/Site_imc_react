562d4922ede45b57b68d2fc1b3d478405471c912
Barrado <69765439+Barrrado@users.noreply.github.com>
2025-04-01 02:57:17 -0300

    Update footer.jsx

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -7,7 +7,7 @@ function Footer () {
     <div className='foot__conteiner'>
         <div className='foot__logo'>
             <div><p className='par__footer'>Este Site e Feito para estudos  do curso EBAC</p></div>
-            <dir className='texto__footer'>Desenvolvido por <a href="">Diego Barrado</a></dir>
+            <dir className='texto__footer'>Desenvolvido por <a href="https://github.com/Barrrado">Diego Barrado</a></dir>
     </div>
     </div>
     </>
